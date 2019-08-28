8197efeee7dacd74d810e5cf51591b24041a84a3
Aaron Conway <aaronconwayphd@gmail.com>
2019-08-28 08:29:30 -0400

    moved presentation folder to personal webpage - easier to deploy online

---
+++ hexSticker/hexSticker.R
@@ -0,0 +1,5 @@
+library(hexSticker)
+sticker(img, package="spiritR", p_size=10, p_color="#f7f8fa", s_x=1, s_y=.75, 
+        s_height=1, s_width=1,
+        h_fill="#b2bcc9", h_color="#002a60",
+        filename="man/figures/logo.png")
